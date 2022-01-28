165fd3e75d7df7cb4aa5722f2c5bb500ff743f0d
tbenjamin1 <tbenpollyl@gmail.com>
2022-01-29 01:59:28 +0200

    implementing delete and update products

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -6,6 +6,53 @@ import './productItem.css'
 
 
 const ProductItem = ({ id, title, price, description, image }) => {
+
+
+let productId = id;
+
+console.log(productId)
+
+
+
+    const deleteProduct =(proId)=>{
+
+          // DELETE request using fetch with error handling
+          axios.get(`https://fakestoreapi.com/products/${proId}`, { method: 'DELETE' })
+         .then((response=>{
+             console.log(response)
+         }))
+          .catch((error) => {
+             
+              console.log('There was an error!', error);
+          });
+
+    }
+
+    const editProduct =(proId)=>{
+
+
+        const updateData ={
+         
+            "title":"updating produtcs",
+             "price":"50",
+              "description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+               
+        }
+
+        // DELETE request using fetch with error handling
+        axios.put(`https://fakestoreapi.com/products/${proId}`, updateData)
+       
+        .then((response=>{
+            console.log(response)
+        }))
+         .catch((error) => {
+            
+             console.log('There was an error!', error);
+         });
+
+  }
+
+
   return (
     <Card>
       <div className="card-content">
@@ -16,8 +63,8 @@ const ProductItem = ({ id, title, price, description, image }) => {
           <h3>${price} </h3>
         </div>
         <div className="card-button">
-          <button type="button">Edit</button>
-          <button type="button">delete</button>
+          <button type="button" onClick={() => editProduct(productId) }>Edit</button>
+          <button type="button" onClick={() => deleteProduct(productId) }>delete</button>
         </div>
       </div>
     </Card>
